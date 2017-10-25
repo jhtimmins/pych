6139f7b7b76209a6affbd0aa6f3cba213cd6cb0b
James Timmins <jameshtimmins@gmail.com>
2017-10-24 21:14:05 -0700

    Add models and migrations.

--- channels/models.py
+++ channels/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Channel(models.Model):
+    name = models.CharField(max_length=250)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ChannelUserLink(models.Model):
+        user = models.ForeignKey(User)
+        channel = models.ForeignKey(Channel)
+        STATUS_CHOICES = (
+            ('invited', 'invited'),
+            ('active', 'active'),
+            ('inactive', 'inactive')
+        )
+        status = models.CharField(choices=STATUS_CHOICES, max_length=8)
+        created_at = models.DateTimeField(auto_now=True)
+        updated_at = models.DateTimeField(auto_now=True)
--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from channels.models import Channel
 
-# Create your models here.
+
+class Message(models.Model):
+    body = models.TextField()
+    user = models.ForeignKey(User)
+    channel = models.ForeignKey(Channel)
+    created_at = models.DateTimeField(auto_now=True)
